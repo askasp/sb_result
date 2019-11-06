b7672aaa049d492e3c22bf93f7eb38b8de1dcef4
aksel stadler kjetsa <akselkjetsa@gmail.com>
2019-11-06 23:33:11 +0100

    Init commit

---
+++ lib/sb_result.ex
@@ -0,0 +1,42 @@
+defmodule SbResult do
+  defmacro left >>> right do
+    quote do
+      (fn ->
+         case unquote(left) do
+           {:ok, x} -> x |> unquote(right)
+           {:error, _} = expr -> expr
+         end
+       end).()
+    end
+  end
+
+  defmacro map(args, func) do
+    quote do
+      (fn ->
+         result = unquote(args) |> unquote(func)
+         {:ok, result}
+       end).()
+    end
+  end
+
+  defmacro tee(args, func) do
+    quote do
+      (fn ->
+         unquote(args) |> unquote(func)
+         {:ok, unquote(args)}
+       end).()
+    end
+  end
+
+  defmacro try_catch(args, func) do
+    quote do
+      (fn ->
+         try do
+           {:ok, unquote(args) |> unquote(func)}
+         rescue
+           e -> {:error, e}
+         end
+       end).()
+    end
+  end
+end
---
+++ test/sb_result_test.exs
@@ -0,0 +1,8 @@
+defmodule SbResultTest do
+  use ExUnit.Case
+  doctest SbResult
+
+  test "greets the world" do
+    assert SbResult.hello() == :world
+  end
+end
